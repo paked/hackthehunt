9cde0b25f675a498483530d56424b014fa8ba416
Harrison <harrison@theshoebridges.com>
2015-09-19 05:54:41 -0400

    Add initial hackthenorth login

--- js/main.js
+++ js/main.js
@@ -1,6 +1,54 @@
 var fb = new Firebase("https://glaring-torch-6697.firebaseio.com/");
 
-fb.set({
-    name: "hello, world party!",
-    party: 102
+var API_HOST = "https://hackerapi.com/v1";
+var API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0MzM5MTUyOTgsImlkIjoyLCJldnQiOlsxXSwidHlwIjoidXNyIn0.USfHFAJ_AYw4hP-wAjiVSWiXbwxPwWLjzzC5oXhVCws";
+
+$(document).ready(function() {
+    $('#login').click(function() {
+        var username = $('#htn_username').val();
+        var password = $('#htn_password').val();
+        var uid = $('#uid').val();
+
+        getToken(username, password, function(data) {
+            if (data.id + '' != uid) {
+                console.log("Not a valid token.");
+                return;
+            }
+            
+            console.log("Welcome,", data.name);
+        });
+    });
 });
+
+getToken = function(username, password, callback) {
+    var req;
+    req = {};
+    req.method = 'POST';
+    req.endpoint = '/auth/user';
+    req.callback = callback;
+    req.payload = {
+        username: username,
+        password: password
+    };
+
+    return makeRequest(req);
+};
+
+makeRequest = function(req) {
+    var callback = req.callback;
+    console.log(req);
+    $.ajax({
+        url: API_HOST + req.endpoint + '?token=' + API_TOKEN,
+        method: req.method,
+        data: JSON.stringify(req.payload),
+        success: function(data) {
+            if (callback === undefined) {
+                console.log(data);
+                return
+            };
+
+            callback(data);
+        }
+    });
+}
+
