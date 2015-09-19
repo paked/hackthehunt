8b834ef8e7a615327b474df667f3b432bcf443f6
Harrison <harrison@theshoebridges.com>
2015-09-19 17:51:12 -0400

    Revert cors

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ var upload = multer({dest: 'uploads/'});
 var app = express();
 
 app.use(express.static('public'));
-
-app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-          next();
-});
-
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
